3b9d5a75f76ac28aecfaba2d1c48ab5fdc2b9b3d
Uttej Kumar <uttejkumarponugoti@gmail.com>
2020-09-16 16:08:11 +0530

    whole application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
-     # Prevent CSRF attacks by raising an exception.
-    # For APIs, you may want to use :null_session instead.
-    before_filter :configure_devise_params, if: :devise_controller?
-    def configure_devise_params
-      devise_parameter_sanitizer.for(:sign_up) do |u|
-        u.permit(:first_name, :last_name, :gender, :email, :password, :password_confirmation)
-      end
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:email,:password,:password_confirmation])
     end
-end
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_053202) do
+ActiveRecord::Schema.define(version: 2020_09_16_074631) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_09_16_053202) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
